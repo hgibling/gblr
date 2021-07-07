be4652b252b20f563f7e88d8fd04a16d13fc70d7
Heather Gibling <heathergibling@gmail.com>
2021-07-07 16:40:19 -0400

    change scoring to include all edit distances, no logging

--- gblr.py
+++ gblr.py
@@ -37,22 +37,26 @@ def get_genotype_names(allele_names):
 
 ### parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--alleles', type=str, required=True, help='fasta file of allele sequences with flanking sequences')
+parser.add_argument('-a', '--alleles', type=str, required=True, help='fasta file of sequences for alleles or region of interest, including flanking sequences')
 parser.add_argument('-r', '--reads', type=str, required=True, help='fasta/q file of sequencing reads')
 parser.add_argument('-f', '--flank-length', type=int, default=10000, help='length of sequences flanking alleles')
-parser.add_argument('-t', '--alignment-tolerance', type=int, default=50, help='minimum number of bases to which a read must align in the variable region of interest')
-parser.add_argument('-m', '--max-mismatch', type=float, default=0.05, help='maximum proportion of a read that can be mismatched/indels relative to an allele')
-parser.add_argument('-d', '--diploid', action='store_true', help='get diploid genotype likelihoods instead of haploid (cannot be used with --quick-count)')
-parser.add_argument('-q', '--quick-count', action='store_true', help='get counts of reads that align best to alleles instead of full likelihood scoring')
-parser.add_argument('-D', '--delimiter', type=str, default='\t', help='delimiter to use for output results')
+parser.add_argument('-t', '--flank-tolerance', type=int, default=50, help='minimum number of bases to which a read must align in the flanking regions')
+parser.add_argument('-d', '--diploid', action='store_true', help='get diploid genotype scores instead of haploid (cannot be used with --quick-count)')
+parser.add_argument('-q', '--quick-count', action='store_true', help='get counts of reads that align best to alleles instead of scores')
+parser.add_argument('-m', '--max-mismatch', type=float, default=0.05, help='for quick count: maximum proportion of a read that can be mismatched/indels relative to an allele')
+parser.add_argument('-T', '--alignment-tolerance', type=int, default=50, help='for quick count: minimum number of bases to which a read must align in the variable region of interest')
+parser.add_argument('-D', '--delimiter', type=str, default='\t', help='delimiter to use for results output')
 args = parser.parse_args()
 
 ### check arguments
+if args.flank_length < args.flank_tolerance:
+    exit("ERROR: flank length must be longer than flank tolerance. Check parameters.")
+
 if args.max_mismatch < 0 or args.max_mismatch > 1:
     exit("ERROR: max-mistmatch argument must be a proportion (between 0 and 1). Check parameters.")
 
 if args.diploid and args.quick_count:
-    exit("ERROR: diploid calling cannot be done with the quick-count methid (haploid only)")
+    exit("ERROR: diploid calling cannot be done with the quick-count methid (haploid only). Check parameters.")
 
 ### get allele and read sequences
 alleles = get_sequences_from_fasta(args.alleles)
@@ -63,17 +67,16 @@ quality_reads = set()
 flank_reads = set()
 bad_reads = set()
 allele_names = list(alleles.keys())
-allele_lengths = dict.fromkeys(allele_names)
+all_allele_lengths = dict.fromkeys(allele_names)
 allele_counts = defaultdict(int)
 edit_distances = []
 
 ### make sure specified flank length not longer than any allele sequences
 for name, sequence in alleles.items():
-    allele_length_no_end_flank = len(sequence) - args.flank_length
-    if allele_length_no_end_flank <= 0:
-        exit("ERROR: flank-length argument too long for allele sequences provided (failed at allele %s)." % allele_name)
+    if len(sequence) <= args.flank_length * 2:
+        exit("ERROR: flank-length argument too long for allele sequence %s" % name)
     else:
-        allele_lengths[name] = allele_length_no_end_flank
+        all_allele_lengths[name] = len(sequence)
 
 ### for generating quick counts
 if args.quick_count:
@@ -91,7 +94,7 @@ if args.quick_count:
 
                 ### ignore alignments that do not meet the minimum number of bases a read must align to in the variable region of interest
                 # check if alignment starts after the 3' minimum alignment threshold, or ends before the 5' minimum threshold
-                if ((result['locations'][0][0] > (allele_lengths[allele_name] - args.alignment_tolerance)) or (result['locations'][0][1] < (args.flank_length + args.alignment_tolerance))):
+                if ((result['locations'][0][0] > (all_allele_lengths[allele_name] - args.flank_length - args.alignment_tolerance)) or (result['locations'][0][1] < (args.flank_length + args.alignment_tolerance))):
                     flank_reads.add(read.name)
                     continue
 
@@ -139,66 +142,79 @@ if args.quick_count:
     for allele, count in sorted(allele_counts.items(), key=lambda x: x[1], reverse=True):
         print(allele, count, count/len(quality_reads), sep=args.delimiter)
 
-### for generating full likelihood scores:
+### for generating scores:
 else:
-    mismatched_proportions = {}
+    all_edit_distances = {}
+    all_cigars = {}
 
     ### align each read against all alleles
     for read in reads:
         read_distance_dict = dict.fromkeys(allele_names)
+        read_cigar_dict = dict.fromkeys(allele_names)
 
         ### check alignment to each allele
         for allele_name, allele_sequence in alleles.items():
-            best_distance = args.max_mismatch * len(read.sequence)
+            best_distance = math.inf
         
             ### check alignment for forward and reverse reads
             for strand_idx, strand_sequence in enumerate([read.sequence, reverse_complement(read.sequence)]):
                 result = edlib.align(strand_sequence, allele_sequence, mode = "HW", task = "path")
 
-                ### ignore alignments that do not meet the minimum number of bases a read must align to in the variable region of interest
-                # check if alignment starts after the 3' minimum alignment threshold, or ends before the 5' minimum threshold
-                if ((result['locations'][0][0] > (allele_lengths[allele_name] - args.alignment_tolerance)) or (result['locations'][0][1] < (args.flank_length + args.alignment_tolerance))):
+
+                ### check that read spans full region of interest and at least args.flank_tolerance into both flank sequences 
+                ### if not, ignore
+                if result['locations'][0][0] > (args.flank_length - args.flank_tolerance) or result['locations'][0][1] < (all_allele_lengths[allele_name] - args.flank_length + args.flank_tolerance):
                     flank_reads.add(read.name)
                     continue
 
-                ### if acceptable edit distance, store lowest proportion of mismatched read bases between forward and reverse read sequences
+                ##### subset read to the portion that aligns to the region of interest
+                subset_start_position = args.flank_length - result['locations'][0][0]
+                subset_end_position = args.flank_length - (all_allele_lengths[allele_name] - result['locations'][0][1]) + 1
+                strand_subset = strand_sequence[subset_start_position : -end]
+
+                ### realign read subset to allele
+                subset_result = edlib.align(strand_subset, allele_sequence[args.flank_length : -args.flank_length], mode = "HW", task = "path")
+
+                ### check edit distance and store lowest edit distance between forward and reverse read sequences
                 if result['editDistance'] <= best_distance:
-                    read_distance_dict[allele_name] = result['editDistance'] / len(read.sequence)
+                    read_distance_dict[allele_name] = result['editDistance']
+                    read_cigar_dict[allele_name] = result['cigar']
                     best_distance = result['editDistance']
             
         ### if acceptable alignment, store read edit proportions for each allele
-        mismatched_proportions[read.name] = read_distance_dict
+        all_edit_distances[read.name] = read_distance_dict
 
-    ### get table of likelihoods         # TODO: deal with null values
-    mismatched_allele_likelihoods = pd.DataFrame.from_dict(mismatched_proportions, orient='index')
+    ### get table of edit distances         # TODO: deal with null values
+    allele_edit_distances = pd.DataFrame.from_dict(all_edit_distances, orient='index')
+    print(allele_edit_distances, file=sys.stderr)
 
-    ### get genotype likelihoods if doing diploid calling
+    ### get genotype edit distances if doing diploid calling
     if args.diploid:
 
         ### get all possible genotypes
         genotype_names = get_genotype_names(allele_names)
-        mismatched_genotype_likelihoods = pd.DataFrame(index=mismatched_allele_likelihoods.index, columns=genotype_names)
+        genotype_edit_distances = pd.DataFrame(index=allele_edit_distances.index, columns=genotype_names)
 
         for g in genotype_names:
             split_alleles = g.split('/')
-            mismatched_genotype_likelihoods[g] = ( mismatched_allele_likelihoods[split_alleles[0]] / 2 ) + ( mismatched_allele_likelihoods[split_alleles[1]] / 2 )
-        
-        ### get likelihoods
-        mismatched_genotype_likelihoods = mismatched_genotype_likelihoods.replace(0, 1)
-        mismatched_likelihoods = np.log10(mismatched_genotype_likelihoods.replace(0, 0.000001)).sum().sort_values()
-    
-    ### get allele likelihoods if not doing diploid calling
+            genotype_edit_distances[g] = ( allele_edit_distances[split_alleles[0]] / 2 ) + ( allele_edit_distances[split_alleles[1]] / 2 )
+
+        print(genotype_edit_distances, file=sys.stderr)
+
+        all_scores = genotype_edit_distances.sum().sort_values()
+
     else:
-        mismatched_likelihoods = np.log10(mismatched_allele_likelihoods.replace(0, 0.0000001)).sum().sort_values()
+
+        all_scores = allele_edit_distances.sum().sort_values()
     
-    ### print results (allele or genotype, likelihood-ish score)
-    for name, likelihood in mismatched_likelihoods.items():
+    ### print results (allele or genotype name, score)
+    for name, likelihood in all_scores.items():
         print(name, likelihood, sep=args.delimiter)
 
 ### print useful information to stderr
-print("Max proportion of read that is mismatches/indels: %d" % args.max_mismatch, file=sys.stderr)
-#print("Edit distance stats: Min: %d, Median: %d, Max: %d" % (ED_min, ED_mean, ED_max), file=sys.stderr)
-print("Number of alleles tested: %d" % len(alleles), file=sys.stderr)
-print("Number of quality reads: %d" % len(quality_reads), file=sys.stderr)
-print("Number of low quality reads: %d" % len(bad_reads), file=sys.stderr)
-print("Number of reads aligning predominantly to flank sequences: %d" % len(flank_reads), file=sys.stderr)
+# print("Max proportion of read that is mismatches/indels: %d" % args.max_mismatch, file=sys.stderr)
+# print("Edit distance stats: Min: %d, Median: %d, Max: %d" % (ED_min, ED_mean, ED_max), file=sys.stderr)
+# print("Number of alleles tested: %d" % len(alleles), file=sys.stderr)
+# print("Number of quality reads: %d" % len(quality_reads), file=sys.stderr)
+# print("Number of low quality reads: %d" % len(bad_reads), file=sys.stderr)
+# print("Number of reads aligning predominantly to flank sequences: %d" % len(flank_reads), file=sys.stderr)
